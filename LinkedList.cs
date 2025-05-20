92ca31053803667f24d8b9dc5f3c319813b22656
4k-1 <149543027+4k-1@users.noreply.github.com>
2025-05-20 23:37:34 +0800

    Add files via upload

---
+++ LinkedList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LinkedList
+{
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+
+        public Node(int data) 
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    private Node head;
+    private int currentNumber;
+
+    public LinkedList(int startNumber)
+    {
+        currentNumber = startNumber;
+        head = null;
+    }
+
+    public void Enqueue()
+    {
+        Node newNode = new Node(currentNumber++);
+        if (head == null)
+            head = newNode;
+        else
+        {
+            Node temp = head;
+            while (temp.Next != null)
+                temp = temp.Next;
+            temp.Next = newNode;
+        }
+    }
+
+    public void Dequeue()
+    {
+        if (head != null)
+            head = head.Next;
+    }
+
+    public List<int> ToList()
+    {
+        List<int> result = new List<int>();
+        Node temp = head;
+        while (temp != null)
+        {
+            result.Add(temp.Data);
+            temp = temp.Next;
+        }
+        return result;
+    }
+    public int PeekLast()
+    {
+        Node temp = head;
+        if (temp == null) return -1;
+
+        while (temp.Next != null)
+            temp = temp.Next;
+
+        return temp.Data;
+    }
+
+
+}
---
+++ QueueMonitorForm.cs
@@ -0,0 +1,25 @@
+namespace queueProject
+{
+    public partial class QueueMonitorForm : Form
+    {
+        public QueueMonitorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
